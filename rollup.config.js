0453fc0623f55d226b5aa9ae7eb79dc98cdf4817
Rubens Mariuzzo <rubens@mariuzzo.com>
2018-03-24 15:59:57 -0400

    Updated: rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,26 @@
 import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify'
+import replace from 'rollup-plugin-replace'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import filesize from 'rollup-plugin-filesize'
 import { minify } from 'uglify-es'
 import pkg from './package.json';
+
+const plugins = [
+  babel({ exclude: ['node_modules/**'] }),
+  resolve(),
+  commonjs(),
+  replace({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) }),
+  uglify({}, minify),
+  filesize(),
+]
 
 export default [
 
   // browser-friendly UMD build
   {
     input: 'src/NewWindow.js',
-    external: ['react', 'react-dom'],
     output: {
       name: 'ReactNewWindow',
       file: pkg.browser,
@@ -20,25 +31,19 @@ export default [
         'react-dom': 'ReactDOM'
       },
     },
-    plugins: [
-      babel(),
-      uglify({}, minify),
-      filesize(),
-    ]
+    external: ['react', 'react-dom'],
+    plugins
   },
 
-  // CommonJS (for Node) and ES module (for bundlers) build.
+  // ES module build.
   {
     input: 'src/NewWindow.js',
+    output: {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
+    },
     external: ['react', 'react-dom'],
-    output: [
-      { file: pkg.main, format: 'cjs', sourcemap: true },
-      { file: pkg.module, format: 'es', sourcemap: true },
-    ],
-    plugins: [
-      babel(),
-      uglify({}, minify),
-      filesize(),
-    ]
+    plugins
   }
 ];
